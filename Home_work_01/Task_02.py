06bc4aacc27e55b485f819799547364e1ca366e0
Nurlan Ormond <sunshineforester@gmail.com>
2023-04-07 23:10:38 +0300

    condition for even input added

--- Home_work_01/Task_02.py
+++ Home_work_01/Task_02.py
@@ -1,7 +1,11 @@
 print ("Input how many origami was made by Petya and Sergey together:")
 s = int(input())
-petyaCraft = s // 2
-sergeyCraft = petyaCraft
-katyaCraft = 2*(petyaCraft + sergeyCraft) 
-print (f"Petya made {petyaCraft} origami, Sergey - {sergeyCraft}, Katya - {katyaCraft}")
-print (f"Total origami: {petyaCraft + sergeyCraft +katyaCraft}")
+if s % 2 != 0:
+    print ("Warning: Input value should be even!\n"
+           "(press CTRL + F5 to try again)")
+else:    
+    petyaCraft = s // 2
+    sergeyCraft = petyaCraft
+    katyaCraft = 2*(petyaCraft + sergeyCraft) 
+    print (f"Petya made {petyaCraft} origami, Sergey - {sergeyCraft}, Katya - {katyaCraft}")
+    print (f"Total origami: {petyaCraft + sergeyCraft +katyaCraft}")
