986be2fb2d37ec7f8777ab3b6f165ea93d5c9243
Nurlan Ormond <sunshineforester@gmail.com>
2023-04-08 02:37:12 +0300

    Input message added

--- Home_work_01/Task_03.py
+++ Home_work_01/Task_03.py
@@ -1,10 +1,11 @@
-ticket = input() 
+
+ticket = input('\n\nInput ticket number: ') 
 list = [int(a) for a in str(ticket)]
 
 if len(list) % 2 != 0:
     print ("Warning: Input value should be even!\n"
-           "(press CTRL + F5 to try again)")
+           "(press CTRL + F5 to try again)\n\n")
 elif sum(list [0 : len(list)//2]) == sum(list [len(list)//2 : len(list)]):
-    print(f"Lucky ticket {sum(list [0 : len(list)//2])} = {sum(list [len(list)//2 : len(list)])}")
+    print(f"Lucky ticket {sum(list [0 : len(list)//2])} = {sum(list [len(list)//2 : len(list)])}\n\n")
 else:
-    print(f"Ordinary ticket {sum(list [0 : len(list)//2])} is not as {sum(list [len(list)//2 : len(list)])}")
+    print(f"Ordinary ticket {sum(list [0 : len(list)//2])} is not as {sum(list [len(list)//2 : len(list)])}\n\n")
