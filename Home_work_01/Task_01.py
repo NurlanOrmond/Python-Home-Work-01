5c1fdc3afc42d6871ecc22b3e623c28ba8a54c9e
Nurlan Ormond <sunshineforester@gmail.com>
2023-04-07 22:13:44 +0300

    Task_1 is done

--- Home_work_01/Task_01.py
+++ Home_work_01/Task_01.py
@@ -1,1 +1,10 @@
-print("Hello World")
+n = 123
+# n = 100
+helper = 0
+third = n % 10
+helper = n // 10
+second = helper % 10
+helper //=  10
+first = helper % 10
+
+print(f"First: {first}  Second: {second} Third: {third} Sum: {first + second + third}")
