419361436ce89a14afbbe4d080269693317548d1
Nurlan Ormond <sunshineforester@gmail.com>
2023-04-14 00:14:12 +0300

    Task 12 is done

---
+++ Home_work_02/Task_12.py
@@ -0,0 +1,8 @@
+sum = int(input("Input sum of two numbers"))
+product = int(input("Input the product of that numbers"))
+limit = 1000
+for i in range(limit):
+    for j in range(limit):
+        if sum == i + j and product == i * j:
+            print(f"The one of numbers is: {i}")
+            print(f"Another one is: {j}")
